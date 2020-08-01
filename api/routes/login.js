3c708c7b81bdc6d280b28431dd4b6f7222356e5a
mjstobo <mjstobo@gmail.com>
2020-08-01 13:26:34 +1000

    Refactor login promise handling and added logic to find / create Zap default playlist

--- api/routes/login.js
+++ api/routes/login.js
@@ -1,8 +1,8 @@
+import { v4 as uuidv4 } from "uuid";
+import querystring from "querystring";
 const express = require("express");
 const axios = require("axios").default;
 const routes = express.Router();
-import { v4 as uuidv4 } from "uuid";
-import querystring from "querystring";
 
 const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
@@ -11,15 +11,17 @@ const redirect_uri = "http://localhost:3000/api/callback";
 //constants
 const stateKey = "spotify_auth_state";
 
-const getUser = (headerOptions) => {
-  axios
+const getUser = async (headerOptions) => {
+   let user = await axios
     .get("https://api.spotify.com/v1/me", {
       headers: headerOptions,
     })
     .then((response) => {
-      return response;
+      let currUser = response.data;
+      return currUser;
     })
     .catch((e) => console.log(e));
+    return user;
 };
 
 const loginToSpotify = (req, res) => {
@@ -27,7 +29,7 @@ const loginToSpotify = (req, res) => {
   res.cookie(stateKey, state);
 
   const scope =
-    "user-read-private user-read-email user-read-currently-playing user-read-playback-state user-modify-playback-state";
+    "user-read-private user-read-email user-read-currently-playing user-read-playback-state user-modify-playback-state playlist-modify-public";
   res.redirect(
     "https://accounts.spotify.com/authorize?" +
       querystring.stringify({
@@ -40,11 +42,11 @@ const loginToSpotify = (req, res) => {
   );
 };
 
-const spotifyCallback = (req, res) => {
+const spotifyCallback = async (req, res) => {
   const state = req.query.state || null;
   const storedState = req.cookies ? req.cookies[stateKey] : null;
 
-  if (state != storedState || state === null) {
+  if (state !== storedState || state === null) {
     res.redirect("/#error");
   } else {
     res.clearCookie(stateKey);
@@ -66,7 +68,7 @@ const spotifyCallback = (req, res) => {
       },
     };
 
-    axios(authOptions)
+    let sessionData = await axios(authOptions)
       .catch((e) => {
         res.redirect(
           "/#" +
@@ -79,16 +81,20 @@ const spotifyCallback = (req, res) => {
         let access_token = response.data.access_token,
           refresh_token = response.data.refresh_token,
           headerOptions = { Authorization: "Bearer " + access_token };
+          return {access_token: access_token, refresh_token: refresh_token, headerOptions: headerOptions};
+      });
 
-        const userDetails = getUser(headerOptions);
-        req.session.currentUser = userDetails;
-        req.session.isSpotifyLoggedIn = true;
-        req.session.access_token = access_token;
-        req.session.session_id = uuidv4();
-        req.session.save(() => {
+      if(sessionData.access_token){
+        await getUser(sessionData.headerOptions).then((currentUser) => {
+          req.session.isSpotifyLoggedIn = true;
+          req.session.access_token = sessionData.access_token;
+          req.session.session_id = uuidv4();
+          req.session.currentUser = currentUser;
+          req.session.save(() => {
           res.redirect("/#");
         });
-      });
+        })
+      }
   }
 };
 
--- api/routes/playlist.js
+++ api/routes/playlist.js
@@ -1,21 +1,53 @@
-const express = require('express');
-const axios = require('axios').default;
+const express = require("express");
+const { response } = require("express");
+const axios = require("axios").default;
 const routes = express.Router();
 
+const getZapPlaylistFromList = async (playlistArray) => {
+  let zapPlaylist = playlistArray.filter(
+    (playlist) => playlist.name === "ZAP::Playlist"
+  );
+  return zapPlaylist ? zapPlaylist : false;
+};
+
+const createZapPlaylist = async (headerOptions, currUserId) => {
+  return await axios
+    .post(
+      `https://api.spotify.com/v1/users/${currUserId}/playlists`,
+      { name: "ZAP::Playlist" },
+      { headers: headerOptions }
+    )
+    .then((response) => {
+      return response.data;
+    })
+    .catch((e) => console.log(e));
+};
+
 const getPlaylists = async (req, res) => {
-  let headerOptions = {
+  const headerOptions = {
     Authorization: "Bearer " + req.session.access_token,
   };
 
-  await axios.get("https://api.spotify.com/v1/me/playlists", {
-        headers: headerOptions
-      })
-      .then(response => {
-        console.log(response.data.items[0])
-      })
-      .catch((e) => console.log(e));
-  }
-
+  let zapPlaylist;
+  let currUserId = req.session.currentUser.id;
+  let response = await axios.get("https://api.spotify.com/v1/me/playlists", { headers: headerOptions })
+    .then((response) => {
+      zapPlaylist = getZapPlaylistFromList(response.data.items);
+      return zapPlaylist;
+    })
+    .catch((e) => console.log(e));
+  
+    if (response.length === 0) {
+      console.log("Playlist doesn't exist. Creating new one");
+      await createZapPlaylist(headerOptions, currUserId).then((p) => {
+        console.log("Playlist created, responding.");
+        res.json(p)
+      });
+    } else {
+      res.json(response);
+    }
+  
+};
 routes.get("/api/playlists", getPlaylists);
-   
+
 module.exports = routes;
