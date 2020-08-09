69b5744c0d09c5c73c029318ba9bea734aa29237
mjstobo <mjstobo@gmail.com>
2020-08-09 16:01:30 +1000

    Updated Theme Keyword API to return synonyms, related nouns and means like words

--- api/routes/themeKeyword.js
+++ api/routes/themeKeyword.js
@@ -1,22 +1,68 @@
-const express = require('express');
-const axios = require('axios').default;
+const express = require("express");
+const { response } = require("express");
+const axios = require("axios").default;
 const routes = express.Router();
 
-const getThemeKeyword = async (req, res) => {
-    let searchTerm = req.query.search;
-    if(searchTerm) {
-      await axios.get('http://api.datamuse.com/words', {
-        params: {
-          ml: searchTerm,
-          max: 25
-        }
-      })
-      .then(response => res.status(200).json(response.data))
-      .catch(e => console.log(e));
-    }
- }
+const returnWordsFromResponse = (responseData) => {
+  return responseData.data.map((data) => data.word);
+};
 
+const getThemeKeyword = async (req, res) => {
+  let searchTerm = req.query.search;
+
+  if (searchTerm) {
+    const synonyms = axios.get("http://api.datamuse.com/words", {
+      params: {
+        rel_syn: searchTerm,
+        max: 15,
+      },
+    });
+
+    const relatedWords = axios.get("http://api.datamuse.com/words", {
+      params: {
+        ml: searchTerm,
+        max: 10,
+      },
+    });
+
+    const nounWords = axios.get("http://api.datamuse.com/words", {
+      params: {
+        rel_jja: searchTerm,
+        max: 5,
+      },
+    });
+
+    const triggerWords = axios.get("http://api.datamuse.com/words", {
+      params: {
+        rel_trg: searchTerm,
+        max: 5,
+      },
+    });
+
+    await axios
+      .all([synonyms, relatedWords, nounWords, triggerWords])
+      .then(
+        axios.spread((...responses) => {
+          const synonymsResponse = responses[0];
+          const relatedWordsResponse = responses[1];
+          const adjWordsResponse = responses[2];
+          const trgWordsResponse = responses[2];
+
+          const mergedData = [
+            ...returnWordsFromResponse(synonymsResponse),
+            ...returnWordsFromResponse(relatedWordsResponse),
+            ...returnWordsFromResponse(adjWordsResponse),
+            ...returnWordsFromResponse(trgWordsResponse)
+          ];
+          const dataToReturn = [...new Set(mergedData)];
+
+          res.status(200).json(dataToReturn);
+        })
+      )
+      .catch((e) => console.log(e));
+  }
+};
 
 routes.get("/api/theme-keyword", getThemeKeyword);
-   
+
 module.exports = routes;
